0540ae4f8c186317c078d3209a58820da6213cd3
Christoph Schulze <schulzch@utrc.utc.com>
2019-05-06 14:45:38 -0700

    added copyright, example source, authors

---
+++ src/ExampleSource.java
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) United Technologies Research Center. All rights reserved.
+ *  See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+public class ExampleSource {
+
+}
