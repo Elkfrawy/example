08d9a9bf58ccfbbfe8daed0874ceef15d5abca7d
Christoph Schulze <schulzch@utrc.utc.com>
2019-05-07 09:16:00 -0700

    updated copyright and in source notation

--- src/ExampleSource.java
+++ src/ExampleSource.java
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) United Technologies Research Center. All rights reserved.
+ *  Copyright (c) 2019 United Technologies Corporation. All rights reserved.
  *  See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
